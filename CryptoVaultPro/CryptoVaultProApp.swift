e8d1a1e35bc5094953ea90e3aa12a123f2dcec0e
Jaanus Siim <jaanus@jaanussiim.com>
2023-12-16 06:03:30 +0200

    Empty project

---
+++ CryptoVaultPro/CryptoVaultProApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoVaultProApp.swift
+//  CryptoVaultPro
+//
+//  Created by Jaanus Siim on 16.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoVaultProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
